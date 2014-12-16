506e1cc6e98e353259829b9b242602bab1fedb33
Craig Grant <craigstephengrant@gmail.com>
2014-12-15 17:20:04 -0800

    dev build/nav build

---
+++ app/js/app.js
@@ -0,0 +1,40 @@
+"use strict";
+
+require.config({
+    baseUrl: './',
+    paths: {
+        jquery: [
+          'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min',
+          '../app/js/vendor/jquery-1.11.0.min'
+        ],
+        jqueryui: [
+          'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min',
+          '../app/js/vendor/jqueryui.min.js'
+        ],
+        modernizr: '../app/js/vendor/modernizr',
+        fastclick: '../app/js/vendor/fastclick',
+        froogaloop: '../app/js/vendor/froogaloop',
+        history: '../app/js/vendor/history',
+        infinitescroll: '../app/js/vendor/infinitescroll',
+        isotope: '../app/js/vendor/isotope',
+        mediaelement: '../app/js/vendor/mediaelement',
+        spin: '../app/js/vendor/spin.min',
+        imagesloaded: '../app/js/vendor/imagesloaded.pkgd.min',
+        masonry: '../app/js/vendor/masonry.pkgd.min',
+        _common: '../app/js/modules/_common',
+        _nav: '../app/js/modules/_nav',
+        _formpages: '../app/js/modules/_formpages'
+    },
+    shim: {
+      'jquery': {
+          exports: '$'
+      },
+      'mediaelement' : ['jquery']
+    }
+});
+
+require(['_common'], function(runMethod) {
+  runMethod.init($(window),$(document));
+});
+
+
---
+++ app/js/modules/_nav.js
@@ -0,0 +1,107 @@
+'use strict';
+
+define(['jquery'], function($) {
+
+  var $win, $doc,
+      $body = $('body'),
+      $main = $('.main');
+
+  var toggleMobileMenu = function() {
+
+
+    if ($body.hasClass('is-active')) {
+      //close mobile drawer
+
+      jQuery('#content').toggleClass('is-disabled');
+
+      jQuery('nav').css('opacity', 0);
+
+    } else {
+      //open mobile drawer
+
+      jQuery('#content').toggleClass('is-disabled');
+
+      // jQuery('#content').css('min-height', jQuery(window).height());
+
+      jQuery('nav').css('opacity', 1);
+
+      //set the width of primary content container -> content should not scale while animating
+      // var contentWidth = jQuery('#content').width();
+
+      //set the content with the width that it has originally
+      // jQuery('#content').css('width', contentWidth);
+
+      //display a layer to disable clicking and scrolling on the content while menu is shown
+      jQuery('#contentLayer').css('display', 'block');
+
+      //disable all scrolling on mobile devices while menu is shown
+      jQuery('#container').bind('touchmove', function (e) {
+          e.preventDefault()
+      });
+
+      //set margin for the whole container with a jquery UI animation
+      jQuery("#container").animate({"marginLeft": ["70%", 'easeOutExpo']}, {
+          duration: 700
+      });
+    }
+
+
+    $body.toggleClass('is-active');
+  };
+
+  var removeMobileMenu = function() {
+    $body.removeClass('is-active');
+  };
+
+  var addEventHandlers = function() {
+
+    $win.resize(function(){
+      if ( $win.outerWidth() > 640 ) {
+        removeMobileMenu();
+      }
+    });
+  };
+
+  return {
+    init: function($w, $d) {
+      $win = $w;
+      $doc = $d;
+
+      addEventHandlers();
+
+      $doc.ready(function() {
+
+          //Open the menu
+          jQuery("#hamburger").click(function () {
+
+              toggleMobileMenu();
+
+          });
+
+          //close the menu
+          jQuery("#Hamburger").click(function () {
+              console.log("hi")
+              //enable all scrolling on mobile devices when menu is closed
+              jQuery('#container').unbind('touchmove');
+
+              //set margin for the whole container back to original state with a jquery UI animation
+              jQuery("#container").animate({"marginLeft": ["-1", 'easeOutExpo']}, {
+                  duration: 700,
+                  complete: function () {
+                      jQuery('#content').css('width', 'auto');
+                      jQuery('#contentLayer').css('display', 'none');
+                      jQuery('nav').css('opacity', 0);
+                      jQuery('#content').css('min-height', 'auto');
+
+                  }
+              });
+          });
+      });
+      console.log('nav loaded');
+    },
+
+    toggleMobileMenu: toggleMobileMenu,
+    removeMobileMenu: removeMobileMenu
+  };
+
+});
